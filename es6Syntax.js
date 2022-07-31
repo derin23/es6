b42d130c2b9b36bbde1cfb63ff4dbac0ec6fa20f
Derin Joseph <derinbino@gmail.com>
2022-07-31 16:46:52 -0500

    const array and obj are reference types

--- es6Syntax.js
+++ es6Syntax.js
@@ -1,2 +1,26 @@
 let name = "Derin";
-console.log(name);
+//console.log(name);
+
+let age = 30;
+if (true){
+    let age = 27;
+}
+// console.log(age);
+
+const NUM = 27;
+//NUM = 29;
+//console.log(NUM);
+
+const AGES = [27, 29, 31];
+//console.log(AGES);
+AGES.push(25);
+//console.log(AGES);
+
+const OBJ = {
+    age:27
+};
+console.log(OBJ);
+
+OBJ.age = 30;
+
+console.log(OBJ);
