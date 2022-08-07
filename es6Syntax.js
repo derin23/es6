de3c9ed14f878070af5b579a98772db4b027da0e
Derin Joseph <derin@Derins-MacBook-Pro.local>
2022-08-07 15:21:41 -0500

    arrow func

--- es6Syntax.js
+++ es6Syntax.js
@@ -30,5 +30,24 @@ function doSome(){
 }
 
 let num;
-doSome();
-console.log(num);
+//doSome();
+//console.log(num);
+
+//arrow functions
+var fn = () => {
+console.log("test")
+};
+
+var fun = ()=> console.log("testing arrow func without brackets")
+//fun();
+
+//arrow func that returns a string is same as a normal func returning a string:
+var arrow = ()=> "arrow return";
+
+arrow();
+
+function regularFunc(){
+    return "regular func return";
+}
+
+regularFunc();
