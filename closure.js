0e645a72a792803a4b5fc53bce1e233e8ee7aa10
derin <derinjoseph23@gmail.com>
2025-01-21 00:04:59 -0600

    always fun to mess with closure

--- closure.js
+++ closure.js
@@ -8,7 +8,52 @@ function createFunction(){
 }
 
 const generatedFunc = createFunction();
-const result = generatedFunc(3);
+const howManyEverFunc = generatedFunc;
+const result = howManyEverFunc(3);
 
-console.log(result);
+//console.log(result);
 
+
+
+function multiplyBy2(num){
+    return num * 2;
+}
+
+// console.log(typeof multiplyBy2);
+
+function callFunction(){
+    return multiplyBy2(2);
+}
+
+// console.log(callFunction());
+
+//Function can be returned from other functions in JS
+// function outer(){
+//     let counter = 0;
+//     function incrementCounter(){
+//         counter++;
+//     }
+
+//     incrementCounter();
+// }
+
+
+//Calling a function outside of the function call in which it was defined
+function outer(){
+    let counter = 0;
+    function incrementCounter() { counter++; console.log(counter)}
+    return incrementCounter;
+}
+
+const myNewFunction = outer();
+myNewFunction();
+myNewFunction();
+
+//C.O.V.E or P.L.S.R.D 
+//Closed Over Variable Environment 
+// Persistent Lexical Scope reference data
+//backpack (not real term) is result of JS being a lexically scopped language
+
+const anotherFunction = outer();
+anotherFunction();
+anotherFunction();
